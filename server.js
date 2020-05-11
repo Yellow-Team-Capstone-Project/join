22be3390b028597f530aa1ae60855fa4d15f5448
Jack-Thornhill <phisherman10@hotmail.com>
2020-05-11 16:03:53 -0500

    body-parse

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
-const request = require('request')
+const request = require("request");
+const bodyParser = require("body-parser");
 
 const app = express();
 const path = require("path");
@@ -13,35 +14,34 @@ const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('/video/token', (req, res) => {
-  request(
-    { url: 'https://localhost:8081' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
-      }
-
-      res.json(JSON.parse(body));
+app.get("/video/token", (req, res) => {
+  request({ url: "https://localhost:8081" }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      return res.status(500).json({ type: "error", message: error.message });
     }
-  )
+
+    res.json(JSON.parse(body));
+  });
 });
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.set("Content-Type", "application/json");
   res.send(
     JSON.stringify({
-      token: token.toJwt()
+      token: token.toJwt(),
     })
   );
 };
 
-const generateToken = config => {
+const generateToken = (config) => {
   return new AccessToken(
     config.twilio.twilioAccountSid,
     config.twilio.twilioApiKeySID,
@@ -81,18 +81,22 @@ app.get("/token", (req, res) => {
   console.log(`issued token for ${identity} in room ${roomName}`);
 });
 
-app.get('/video/token', (req, res) => {
+app.get("/video/token", (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
-  const token = videoToken(identity, room, {twilio:{twilioAccountSid,twilioApiKeySID,twilioApiKeySecret}});
+  const token = videoToken(identity, room, {
+    twilio: { twilioAccountSid, twilioApiKeySID, twilioApiKeySecret },
+  });
   sendTokenResponse(token, res);
 });
 
-app.post('/video/token', (req, res) => {
-  console.log("What is req", req.body)
+app.post("/video/token", (req, res) => {
+  console.log("What is req", req.body);
   const identity = req.body.identity;
   const room = req.body.room;
-  const token = videoToken(identity, room, {twilio:{twilioAccountSid,twilioApiKeySID,twilioApiKeySecret}});
+  const token = videoToken(identity, room, {
+    twilio: { twilioAccountSid, twilioApiKeySID, twilioApiKeySecret },
+  });
   sendTokenResponse(token, res);
 });
 
